9731b2bc5ce097e1457d1483f5eb76913e4fe753
albertpb <palumbo.alb@gmail.com>
2023-11-09 17:30:05 -0300

    remove separator and modify chevron icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -29,11 +29,7 @@ export const ChevronIcon: React.FC<Props> = ({ className = "" }) => {
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
         </svg>
     );
 };
